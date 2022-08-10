6f3806a65f6ca91e3730ced4f5ef24a95a0f2860
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-08-10 10:34:24 -0500

    logical calculation center completed

--- figuras/index.js
+++ figuras/index.js
@@ -8,21 +8,24 @@ $buttonCuadrado.addEventListener('click' , calcularPerimetroCuadrado);
 function calcularPerimetroCuadrado() {
     // Se captura el valor del input.
     const inputValue = document.getElementsByClassName('input-cuadrado')[0].value;
-
+    console.log(inputValue)
     // Se hacen los cálculos del área y el cuadrado.
-    const areaCuadrado = (lado) => {lado ** 2};
-    const perimetroCuadrado = (lado) => {lado * 4};
+    
+    const areaCuadrado = (lado) => { return lado ** 2};
+    const perimetroCuadrado = (lado) => { return lado * 4};
     
     // Se llaman los elementos a modificar y se lanzan los cálculos.
+    const resultArea = areaCuadrado(inputValue);
+    const resultPerimetro = perimetroCuadrado(inputValue);
 
     //! Área.
     document.getElementsByClassName('div-calcular-area')[0].innerHTML = `
-        <p>${areaCuadrado((inputValue))} CM^2</p>
+        <p>El valor del área es ${resultArea} CM^2</p>
     `;
 
     //! Perímetro.
     document.getElementsByClassName('div-calcular-perimetro')[0].innerHTML = `
-        <p>${perimetroCuadrado((inputValue))}</p>
+        <p>El valor del perímetro es ${resultPerimetro}</p>
     `;
 };
 
