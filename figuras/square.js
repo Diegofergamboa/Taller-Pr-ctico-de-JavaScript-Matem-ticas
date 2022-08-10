133c6cd986efd46062fd79a729751e566a71c639
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-08-09 19:21:10 -0500

    fixing bugs of the cuadrado section

--- figuras/square.js
+++ figuras/square.js
@@ -3,17 +3,4 @@ function Square(mensaje) {
 }
 
 
-{/* <div class="card-cuadrado">
-<div class="cuadrado">
-    <h2 class="card-h2">Cálculos del cuadrado</h2>
-    <img class="image-cuadrado" src="https://www.abcfichas.com/wp-content/uploads/2020/12/Formula-Area-Cuadrado.jpg" />
-    <input type="text" class="input-cuadrado" placeholder="Ingrese uno de los lados"/>
-    <button class="button-cuadrado" type="button">Calcular</button>
-</div>
-<div class="div-calcular">
-    <div class="div-calcular-perimetro">Perímetro</div>
-    <div class="div-calcular-area">Área</div>
-</div>
-</div> */}
-
 export { Square } ;
