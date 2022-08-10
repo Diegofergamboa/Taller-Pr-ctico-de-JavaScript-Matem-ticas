a25228c7d5f32d8f312e9231bbbf3c3b64e32526
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-08-10 18:34:37 -0500

    layout from the triangulo completed

--- figuras/index.js
+++ figuras/index.js
@@ -1,11 +1,6 @@
 // Principal setter
 import { Square } from "./square.js";
 
-const $setterCalculator = document.getElementsByClassName('setter-calculator');
-
-function clicks(){
-    console.log('di click');
-}
 
 
 const $buttonCuadrado = document.getElementsByClassName('button-cuadrado')[0];
