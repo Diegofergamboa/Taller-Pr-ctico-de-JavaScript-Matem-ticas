13edbdf10cf790bbb3f7602c682e8914d350ae6e
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-08-11 17:09:43 -0500

    circle algorthm completed

--- figuras/index.js
+++ figuras/index.js
@@ -1,8 +1,7 @@
 // Principal setter
-// import { Circle } from "./circle.js";
+import { Circle } from "./circle.js";
 import { Square } from "./square.js";
 import { Triangle } from "./triangle.js";
-// import { Circle } from "./circle.js";
 
 
 // const $buttonCuadrado = document.getElementsByClassName('button-cuadrado')[0];
@@ -12,42 +11,10 @@ import { Triangle } from "./triangle.js";
 // $buttonTriangulo.addEventListener('click' , Triangle);
 
 
-//! pa abajo.
+// const $buttonCirculo = document.getElementsByClassName('button-circulo')[0];
+// $buttonCirculo.addEventListener('click', Circle);
 
 
-// 1.  Capturar el botón de "calcular". (inicializador).
-const $buttonCirculo = document.getElementsByClassName('button-circulo')[0];
-$buttonCirculo.addEventListener('click', Circle)
-
-function Circle(){
-    // 2.  Capturar los valores del "input" (radio).
-    const inputRadio = document.getElementsByClassName('input-circulo')[0].value;
-    const radioNumber = parseInt(inputRadio);
-    
-    // 3. Hacer los calculos.
-    const PI = Math.PI;
-    // const perimetroCirculo = 2 * PI * radioNumber;
-
-
-    function perimetroCirculo(radio) {
-        return 2 * PI * radio ;    
-    };
-
-    const areaCirculo = (radio) => {
-        return PI * (radio ** 2);
-    };
-    
-}
-
-
-
-
-// * construir las variables.
-// * construir las funciones.
-// 4. Imprimir en el DOM.
-// * identificarlo donde está.
-// * ejecutar las funciones de cálculo.
-// * imprimir el DOM los resultados.
 
 
 
