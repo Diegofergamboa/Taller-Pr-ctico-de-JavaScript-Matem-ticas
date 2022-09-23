b19f9a73b026053e08d70f66e891637d44dfeaae
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-09-23 13:22:52 -0500

    updated final project

---
+++ figuras/descuentos-app/descuentos.js
@@ -0,0 +1,18 @@
+// Variables setup.
+
+const cuponDiscounts = [
+    {code : 'D01' , valueDiscount : 10 } ,
+    {code : 'D02' , valueDiscount : 20 } ,
+    {code : 'D03' , valueDiscount : 30 } ,
+    {code : 'D04' , valueDiscount : 40 } ,
+    {code : 'D05' , valueDiscount : 50 } ,
+];
+
+const $codeValue = document.querySelector('#cupon').value;
+const $priceValue = document.querySelector('#precio').value;
+
+// Algorithm for filter.
+
+const resultFilter = cuponDiscounts.map(discount => {
+
+})
