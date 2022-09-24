139c9975384d047d9434228e8ad5b2643b92ce7e
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-09-23 20:21:01 -0500

    updated working on the filter Function

--- figuras/descuentos-app/descuentos.js
+++ figuras/descuentos-app/descuentos.js
@@ -8,11 +8,27 @@ const cuponDiscounts = [
     {code : 'D05' , valueDiscount : 50 } ,
 ];
 
-const $codeValue = document.querySelector('#cupon').value;
-const $priceValue = document.querySelector('#precio').value;
+const $code = document.querySelector('#cupon');
+const $price = document.querySelector('#precio');
+
+const codeValue = $code.value;
+const priceValue = $price.value;
 
 // Algorithm for filter.
+let resultDiscount = 0;
+const resultFilter = () => cuponDiscounts.filter(cupon => {
+    if (cupon.code === codeValue) {
+        console.log('sirvio');
+    }
+});
 
-const resultFilter = cuponDiscounts.map(discount => {
-    cuponDiscounts.filter
-})
+
+const $buttonCalculationdocument = document.getElementById('calculation');
+$buttonCalculationdocument.addEventListener('click', resultFilter)
+
+
+// // Intentar sacarlo con un método filter.
+// Luego con un método find.
+// Luego con un método find y filter con una función como condicional .
+// Hacer el .map que tenga como parámetro una función .filter.
+
