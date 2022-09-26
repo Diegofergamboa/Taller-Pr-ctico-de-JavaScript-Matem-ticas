fd806b156bafd986ef778253307d6f1c07692dc6
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-09-26 18:34:24 -0500

    working on the setting of the algirthm

--- figuras/descuentos-app/descuentos.js
+++ figuras/descuentos-app/descuentos.js
@@ -13,13 +13,31 @@ const couponDiscounts = [
 
 
 
-const printDiscount = () => {
+const calculationDiscount = () => {
+    // Principal variables setting.
     const $userCoupon = document.getElementById('coupon');
     const cuponValue = $userCoupon.value;
+    
+    const $priceUser = document.getElementById('price');
+    const userPriceValue = $priceUser.value;
+    
+    const $printDiscount = document.getElementsByClassName('print-discount');
+
+    // Checker of the code coupon.
+    const checkCodeCoupon = (codeCoupon) => {
+        if (codeCoupon) { // ¿Should i return a true or false in this validation?
+            codeCoupon === cuponValue ;
+        } else {
+            $printDiscount[0].innerHTML = ` El cupón no aplica, el precio sigue siendo ${userPriceValue}`
+        }
+    };
+
+    // Find function.
     const checkDiscount = () => (couponDiscounts.find(couponList => couponList === cuponValue));
-}
+    // Calculation function.
+}   
 
 
 
 const $calculation = document.getElementById('calculation');
-$calculation.addEventListener('click', printDiscount); 
+$calculation.addEventListener('click', calculationDiscount); 
