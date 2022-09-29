5e4ff5bde178287d7120262522c43bfc21e4c8a2
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-09-28 19:13:05 -0500

    algorithm to fin the index position depending if the array is even or odd on its length

--- estadistica-app/estadistica.js
+++ estadistica-app/estadistica.js
@@ -1,5 +1,7 @@
-const arrayTest = [0,1,2,3,3,4,5,6,7,56,56,54];
+// ! Explanation of the reduce method.
 
+const arrayTest = [1,2,3,4,5,6,7];
+console.log(arrayTest.length);
 // Esta función se va a enviar como parámetro.
 
 function reduceEngine(initialValue, SumValue){
@@ -10,3 +12,28 @@ function reduceEngine(initialValue, SumValue){
 
 // Here we put the function of how the engine works in the reduce method 
 const reduceMethodWithArrays = arrayTest.reduce(reduceEngine);
+
+// ! End of the explanation of the reduce method.
+
+
+//! Calculation of the median
+
+// Second challenge to know if a number is odd or even.
+const checkEven = (number) => (number % 2 != 0) ? false : true;
+
+function calculatePosition(array) {
+    let positionA;
+    let positionB;
+    
+    if (!checkEven(array.length)) {
+        positionA = Math.floor(array.length / 2);
+    } else {
+        positionA = Math.floor(array.length / 2);
+        positionB = Math.floor(array.length / 2) + 1;
+    }
+
+    console.log(positionA);
+    console.log(positionB);
+};
+
+
